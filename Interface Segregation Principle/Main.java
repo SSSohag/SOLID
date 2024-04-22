fccd761e69e1d9a339a3bfddf2ba6c6236819c17
Sohag Chandra <146931965+SSSohag@users.noreply.github.com>
2024-04-22 11:39:58 +0600

    Add files via upload

--- Interface Segregation Principle/Main.java
+++ Interface Segregation Principle/Main.java
@@ -1,24 +1,23 @@
-/*
-The Interface Segregation Principle (ISP) states that a 
+/*The Interface Segregation Principle (ISP) states that a 
 client should not be forced to implement an interface that it doesn't use.
 Instead of one large interface, clients should have specific interfaces to adhere to. 
+
 */
 
 // The Worker interface represents a worker
+/*
 interface Worker {
     void work();
     void eat();
 }
-
-// The WorkerWithBreak interface represents a worker who takes breaks
-interface WorkerWithBreak extends Worker {
-    void takeBreak();
+*/
+interface Worker {
+    void work();
+}
+interface Eater {
+    void eat();
 }
 
-// The WorkerWithBonus interface represents a worker who receives a bonus
-interface WorkerWithBonus extends Worker {
-    void receiveBonus(double bonusAmount);
-}
 
 // The Robot class represents a robot worker
 class Robot implements Worker {
@@ -35,7 +34,7 @@ class Robot implements Worker {
 }
 
 // The Human class represents a human worker
-class Human implements WorkerWithBreak, WorkerWithBonus {
+class Human implements Worker, Eater {
     @Override
     public void work() {
         System.out.println("Human is working");
@@ -46,15 +45,6 @@ class Human implements WorkerWithBreak, WorkerWithBonus {
         System.out.println("Human is eating");
     }
 
-    @Override
-    public void takeBreak() {
-        System.out.println("Human is taking a break");
-    }
-
-    @Override
-    public void receiveBonus(double bonusAmount) {
-        System.out.println("Human received a bonus of $" + bonusAmount);
-    }
 }
 
 public class Main {
